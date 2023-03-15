e5b5b3113c3df3a4583a9b3cb738b17ccb620381
Adrian Grimm <usmcamgrimm@gmail.com>
2023-03-15 14:41:06 -0400

    Create a 'coming soon' landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import background from "./images/bgImage.jpg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ 
+      backgroundImage: `url(${background})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      backgroundSize: 'cover'
+    }}>
+      <div 
+        className="App"
+      >
+        <h1>
+          Coming Soon
+        </h1>
+      </div>
     </div>
   );
 }
